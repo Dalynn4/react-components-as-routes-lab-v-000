64566c8976baad91e36e618d3cfc7a4e78b0ddd1
Randall Robinson <keith_adams007@yahoo.com>
2018-08-05 23:47:36 +0000

    automatically backed up by learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,9 @@ const NavBar = () => {
   return (
     <div>
       <NavLink to='/' exact>Home</NavLink>
-
+      <NavLink to='/movies' exact>Movies</NavLink>
+      <NavLink to='/actors' exact>Actors</NavLink>
+      <NavLink to='/directors' exact>Directors</NavLink>
     </div>
   );
 };
